e3a65f45c59fcaf19fb49f0df0dd9ba82b9efa3f
Dania <dania.liu@ocsbstudent.ca>
2021-03-26 17:52:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// Sets variables to default value (0).
+let diameter = 0
+let area = 0
+let circumference = 0
+
+// Connects JavaScript to HTML button.
+document.getElementById('button').addEventListener('click', calculate)
+
+// Determines what the function does when activated.
+function calculate () {
+  // Takes the value of the diameter input box and stores it as an integer.
+  diameter = document.getElementById('D').value
+  diameter = parseInt(diameter)
+  // Uses the formula to calculate the area and displays it on the screen.
+  area = Math.PI * ((diameter / 2) * (diameter / 2))
+  document.getElementById('areaAnswer').innerHTML = area
+  // Uses the formula to calculate the circumference and displays it on the screen.
+  circumference = 2 * Math.PI * (diameter / 2)
+  document.getElementById('circumferenceAnswer').innerHTML = circumference
+}
